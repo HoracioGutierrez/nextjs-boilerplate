801d94bc5fff4429d999f8a0b0b8471bf5c586f0
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-12 03:55:05 -0300

    Implement signup form with email and password inputs; add server-side handling for form submission

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,18 @@
+import Form from 'next/form'
+
 function SignupPage() {
+
+    const handleSignUp = async (formData: FormData) => {
+        "use server";
+        console.log(formData)
+    }
+
     return (
-        <div>SignupPage</div>
+        <Form action={handleSignUp}>
+            <input type="text" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="submit" value="Sign Up" />
+        </Form>
     )
 }
 
