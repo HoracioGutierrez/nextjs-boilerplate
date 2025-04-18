1be73343d2c77a3e04c22e1912052e79aa9fb13f
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-04-18 14:13:49 -0300

    Enhance UI and responsiveness with new dependencies and component updates
    - Added `fluid-tailwind`, `tailwind-clamp`, and `uniweb-tailwind-plugin` dependencies for improved responsive design and text clamping.
    - Updated global CSS to incorporate new plugins and configurations.
    - Refactored button components in the navigation and header for better responsiveness and consistency.
    - Adjusted layout in various components to utilize new clamping utilities for improved text sizing.
    - Enhanced the Drawer and ModeToggle components for better user interaction and visual consistency.

--- components/main-layout/header.tsx
+++ components/main-layout/header.tsx
@@ -2,8 +2,8 @@ import Nav from "./nav";
 
 function Header() {
 	return (
-		<header className="p-2 md:p-4 flex justify-between items-center">
-			<h1 className="font-bold text-md-xl-clamp-md-lg">Boilerplate APP</h1>
+		<header className="px-3-4-clamp py-1-2-clamp flex justify-between items-center">
+			<h1 className="font-bold clamp-[text,16px,24px]">Boilerplate APP</h1>
 			<Nav />
 		</header>
 	);
